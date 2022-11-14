c70a46569373bb44d6c2c42944d3faa8fde733e6
Matthew Sevey <mjsevey@gmail.com>
2022-11-14 08:54:25 -0500

    Minor TODO for PR

--- src/App.js
+++ src/App.js
@@ -14,6 +14,7 @@ function App() {
         <Route path="/" element={<HomePage />} />
         <Route path="/about" element={<About />} />
         <Route path="/contact" element={<Contact />} />
+        {/* <Route path="/refer" element={<Refer />} /> TODO */}
       </Routes>
       <Footer />
     </>
