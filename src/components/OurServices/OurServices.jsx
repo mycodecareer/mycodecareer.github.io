3ec5dae3800313281aa677420066435a9521c4a8
Matthew Sevey <mjsevey@gmail.com>
2023-04-16 22:04:53 -0400

    OurServices: refactor components into ServiceRow

--- src/components/OurServices/OurServices.jsx
+++ src/components/OurServices/OurServices.jsx
@@ -15,72 +15,91 @@ const OurServices = () => {
       <HeaderNormal text="Our Services" />
       <Layout>
         <section className="flex flex-col justify-between items-center w-full lg:gap-24 lg:mt-12 lg:mb-12">
-          <div className="flex flex-col-reverse justify-evenly items-center gap-6 w-full lg:flex-row-reverse lg:justify-between lg:gap-24 ">
-            <div className="flex flex-col justify-start items-start flex-wrap text-left gap-8 w-full lg:w-1/2">
-              <h3 className="text-4xl font-bold md:text-5xl md:pr-10 md:font-normal">
-                Career Coaching
-              </h3>
-              <div className="flex flex-col justify-center items-start gap-2 w-full mb-12">
-                <BulletPoint
-                  image={<RiTreasureMapLine className="fill-secondary" />}
-                  text={"Determine your roadmap for career success"}
-                />
-                <BulletPoint
-                  image={<MdPhoneInTalk className="fill-secondary" />}
-                  text={"Learn how to book more interviews with less applying"}
-                />
-                <BulletPoint
-                  image={<BsSpeedometer2 className="fill-secondary" />}
-                  text={
-                    "Accelerate your growth with accountability and mentorship"
-                  }
-                />
-              </div>
-            </div>
-            <div className="flex flex-1 justify-center items-center aspect-square">
-              <img
-                src={CollabOne}
-                alt="#"
-                className="w-full h-full rounded-md"
-              />
-            </div>
-          </div>
+          {/* First Service */}
+          {ServiceRow(
+            "Career Coaching",
+            [
+              {
+                image: <RiTreasureMapLine className="fill-secondary" />,
+                text: "Determine your roadmap for career success",
+              },
+              {
+                image: <MdPhoneInTalk className="fill-secondary" />,
+                text: "Learn how to book more interviews with less applying",
+              },
+              {
+                image: <BsSpeedometer2 className="fill-secondary" />,
+                text: "Accelerate your growth with accountability and mentorship",
+              },
+            ],
+            CollabOne
+          )}
 
           {/* Second Service */}
-          <div className="flex flex-col-reverse justify-between items-start w-full lg:flex-row lg:gap-24 lg:items-center">
-            <div className="flex flex-col justify-center items-end text-right gap-8 w-full lg:w-1/2">
-              <h3 className="text-4xl font-bold md:text-5xl md:pl-10 md:font-normal">
-                {" "}
-                One-on-one live mentorship{" "}
-              </h3>
-              <div className="flex flex-col justify-center items-end gap-2 w-full">
-                <BulletPoint
-                  image={<VscDebug className="fill-secondary" />}
-                  text={"Debug with the help of an expert"}
-                />
-                <BulletPoint
-                  image={<MdOutlinePersonPin className="fill-secondary" />}
-                  text={"Personalize your learning experience"}
-                />
-                <BulletPoint
-                  image={<RiQuestionAnswerLine className="fill-secondary" />}
-                  text={"Get answers to complex problems"}
-                />
-              </div>
-            </div>
-            <div className="flex flex-1 justify-center items-center aspect-square">
-              <img
-                src={CollabTwo}
-                alt="#"
-                className="w-full h-full rounded-md"
-              />
-            </div>
-          </div>
+          {ServiceRow(
+            "One-on-one live mentorship",
+            [
+              {
+                image: <VscDebug className="fill-secondary" />,
+                text: "Debug with the help of an expert",
+              },
+              {
+                image: <MdOutlinePersonPin className="fill-secondary" />,
+                text: "Personalize your learning experience",
+              },
+              {
+                image: <RiQuestionAnswerLine className="fill-secondary" />,
+                text: "Get answers to complex problems",
+              },
+            ],
+            CollabTwo,
+            true
+          )}
         </section>
       </Layout>
     </section>
   );
 };
+
+function ServiceRow(title, bulletPoints, collabImg, reverse = false) {
+  const baseOuterDivCSS =
+    "justify-evenly items-center gap-6 lg:flex-row-reverse lg:justify-between";
+  const reverseouterdivcss =
+    "justify-between items-start lg:flex-row lg:items-center";
+
+  let outerDivCSS = reverse ? reverseouterdivcss : baseOuterDivCSS;
+
+  let innerDivCSS = reverse
+    ? "justify-center items-end text-right"
+    : "justify-start items-start text-left flex-wrap";
+
+  let bulletCSS = reverse ? "items-end" : "items-start";
+  let h3CSS = reverse ? "pl-10" : "pr-10";
+
+  return (
+    <div className={`flex flex-col-reverse w-full lg:gap-24 ${outerDivCSS}`}>
+      <div className={`flex flex-col gap-8 w-full lg:w-1/2 ${innerDivCSS}`}>
+        <h3
+          className={`text-4xl font-bold md:text-5xl md:font-normal md:${h3CSS}`}
+        >
+          {title}
+        </h3>
+        <div
+          className={`flex flex-col justify-center gap-2 w-full mb-12 ${bulletCSS}`}
+        >
+          {bulletPoints.map((bp) => {
+            return (
+              <BulletPoint key={bp.text} image={bp.image} text={bp.text} />
+            );
+          })}
+        </div>
+      </div>
+      <div className="flex flex-1 justify-center items-center aspect-square">
+        <img src={collabImg} alt="#" className="w-full h-full rounded-md" />
+      </div>
+    </div>
+  );
+}
 
 export default OurServices;
 
