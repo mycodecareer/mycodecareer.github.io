455217e2e6c01a115399ee927c54e031f28e1464
Matthew Sevey <mjsevey@gmail.com>
2023-04-19 15:54:14 -0400

    utils: create utils and create ScrollToTop util

--- src/App.js
+++ src/App.js
@@ -1,21 +1,26 @@
 import React from "react";
 import { Routes, Route } from "react-router-dom";
+import ScrollToTop from "./components/utils/ScrollToTop";
+
+// Components
 import Footer from "./components/common/Footer/Footer";
+import Navbar from "./components/common/Navbar/Navbar";
+
+// Pages
 import About from "./pages/About";
 import Contact from "./pages/Contact";
-import Navbar from "./components/common/Navbar/Navbar";
 import HomePage from "./pages/HomePage";
 import PrivacyPolicy from "./pages/PrivacyPolicy";
 
-import ResourcePage from "./pages/Resources/ResourcePage";
-
 // Pull in resource pages
+import ResourcePage from "./pages/Resources/ResourcePage";
 import resourcePages from "./pages/Resources/resources.json";
 import getResourceFunction from "./pages/Resources/ResourcePageFunctions";
 
 function App() {
   return (
     <>
+      <ScrollToTop />
       <Navbar />
       <Routes>
         <Route path="/" element={<HomePage />} />
