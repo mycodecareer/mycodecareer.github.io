4f4a945678bde6ed71ce069d7b97701bbab53e22
Matthew Sevey <mjsevey@gmail.com>
2023-04-24 13:09:54 -0400

    Replace BrowserRouter with HashRouter

--- src/index.js
+++ src/index.js
@@ -1,14 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
+// import { BrowserRouter } from "react-router-dom";
 import "./index.css";
 import App from "./App";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <BrowserRouter basename={process.env.PUBLIC_URL}>
+    <HashRouter basename={process.env.PUBLIC_URL}>
       <App />
-    </BrowserRouter>
+    </HashRouter>
   </React.StrictMode>
 );
