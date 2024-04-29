ca22221f84317216ad4a4f2cdadd6bdb19324fb5
Matthew Sevey <mjsevey@gmail.com>
2024-04-28 21:46:08 -0400

    feat: add header to testimonials

--- src/pages/Testimonials/Testimonials.jsx
+++ src/pages/Testimonials/Testimonials.jsx
@@ -1,6 +1,9 @@
+import HeaderNormal from "../../components/common/SectionHead/HeaderNormal";
+
 const Testimonials = () => {
   return (
     <section className="container mx-auto px-4 py-8 mt-24">
+      <HeaderNormal text="Testimonials" />
       <iframe
         title="wall-of-love"
         height="800px"
