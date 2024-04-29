914bb6ed1013062e3525a9a75bd9d5d4efa3dad4
Matthew Sevey <mjsevey@gmail.com>
2024-04-28 22:00:59 -0400

    feat: overhaul homepage

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -3,18 +3,27 @@ import CTA from "../components/common/CTA/CTA";
 import LinkButton from "../components/common/CTA/LinkButton";
 import Hero from "../components/Hero/Hero";
 import OurServices from "../components/OurServices/OurServices";
+import Testimonials from "./Testimonials/Testimonials";
+import { JobSearchStrategySessionLink } from "../constants/constants";
+import HeaderNormal from "../components/common/SectionHead/HeaderNormal";
+import { BioOneCTA } from "./About";
 
 const HomePage = () => {
   return (
     <>
       <Hero />
       <OurServices />
-      <CTA
-        text="Learn how we can help you reach your coding goals, schedule a call
-          today!"
-      >
-        <LinkButton text="Get Started" type="secondary" />
+      <HeaderNormal text="Where to Start" />
+      <CTA text="Learn how we can help you reach your coding goals, schedule a FREE Job Search Strategy Session today!">
+        <LinkButton
+          text="Book a Call"
+          type="secondary"
+          link={JobSearchStrategySessionLink}
+        />
       </CTA>
+      <Testimonials />
+      <HeaderNormal text="About Matthew" />
+      <BioOneCTA />
     </>
   );
 };
